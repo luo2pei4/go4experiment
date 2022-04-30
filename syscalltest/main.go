5e80310b9251cc66c2e65049ef2076766cc15d45
luo2pei4 <luo2pei4321@outlook.com>
2022-04-30 14:36:31 +0800

    将内核监听改为协程方式

--- syscalltest/main.go
+++ syscalltest/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"go4experiment/syscalltest/test"
-	"sync"
+	"strings"
 	"syscall"
 )
 
 func main() {
+
 	udev, err := test.NewNetLinker(
 		syscall.RTNLGRP_LINK|syscall.RTNLGRP_IPV4_IFADDR|syscall.RTNLGRP_IPV6_IFADDR,
 		syscall.NETLINK_KOBJECT_UEVENT,
@@ -16,19 +17,11 @@ func main() {
 		fmt.Println(err.Error())
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func(linker *test.NetLinker, wg *sync.WaitGroup) {
-		for {
-			// 此处调用RecvUdevEvent会产生阻塞
-			msg, err := linker.RecvUdevEvent()
-			if err != nil {
-				fmt.Println(err.Error())
-				wg.Done()
-			} else {
-				fmt.Println(msg)
-			}
-		}
-	}(udev, &wg)
-	wg.Wait()
+	knlMsgChan := make(chan []byte)
+	go udev.RecvUdevEvent(knlMsgChan)
+
+	for msg := range knlMsgChan {
+		str := strings.ReplaceAll(string(msg), "\x00", " ")
+		fmt.Println(strings.TrimSpace(str))
+	}
 }
--- syscalltest/test/read.go
+++ syscalltest/test/read.go
@@ -10,6 +10,7 @@ type NetLinker struct {
 }
 
 func NewNetLinker(groups, proto int) (*NetLinker, error) {
+
 	s, err := syscall.Socket(syscall.AF_NETLINK, syscall.SOCK_RAW, proto)
 	if err != nil {
 		return nil, fmt.Errorf("socket: %s", err)
@@ -20,6 +21,7 @@ func NewNetLinker(groups, proto int) (*NetLinker, error) {
 		Pid:    uint32(0),
 		Groups: uint32(groups),
 	}
+
 	err = syscall.Bind(s, saddr)
 	if err != nil {
 		return nil, fmt.Errorf("bind: %s", err)
@@ -28,14 +30,13 @@ func NewNetLinker(groups, proto int) (*NetLinker, error) {
 	return &NetLinker{fd: s}, nil
 }
 
-func (l *NetLinker) RecvUdevEvent() (string, error) {
-	defer func() {
-		recover()
-	}()
-	pkt := make([]byte, 2048)
-	_, err := syscall.Read(l.fd, pkt)
-	if err != nil {
-		return "", fmt.Errorf("read: %s", err)
+func (l *NetLinker) RecvUdevEvent(knlMsgChan chan []byte) {
+	for {
+		pkt := make([]byte, 2048)
+		_, err := syscall.Read(l.fd, pkt)
+		if err != nil {
+			fmt.Printf("syscall.Read error: %s", err)
+		}
+		knlMsgChan <- pkt
 	}
-	return string(pkt), nil
 }
